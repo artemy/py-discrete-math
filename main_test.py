65e6018c3082b407bc0db94083fb89b67102a19f
Artem Makarov <artemy.makarov@gmail.com>
2020-08-28 17:51:11 +0200

    Update test data for inverse

--- main_test.py
+++ main_test.py
@@ -31,10 +31,8 @@ class TestMatrix(unittest.TestCase):
         self.assertEqual(expected, compose(test_set1, test_set2))
 
     def test_inverse(self):
-        test_set = [[1, 3], [2, 2], [2, 3], [2, 4], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [4, 2], [4, 3], [4, 4],
-                    [5, 3]]
-        expected = [[1, 3], [2, 2], [2, 3], [2, 4], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [4, 2], [4, 3], [4, 4],
-                    [5, 3]]
+        test_set = [[1, 3], [2, 4], [5, 2]]
+        expected = [[2, 5], [3, 1], [4, 2]]
         self.assertEqual(expected, inverse(test_set))
 
     def test_transitive(self):
